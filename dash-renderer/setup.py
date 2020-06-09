3a1a753b39d45accb1172bb75e986648e49e99d2
noisycomputation <noisycomputation@gmail.com>
2020-06-09 01:45:18 -0600

    Fix url in setup.py

--- dash-renderer/setup.py
+++ dash-renderer/setup.py
@@ -6,16 +6,18 @@ with open("package.json") as f:
 
 package_name = str(package["name"].replace(" ", "_").replace("-", "_"))
 
+repo_url='https://github.com/noisycomputation/dash'
+
 setup(
     name=package_name,
     version=package["version"],
     author=package["author"],
     packages=[package_name.replace("_noisycomputation", "")],
-    url='https://github.com/noisycomputation/dash',
+    url=repo_url,
     include_package_data=True,
     license=package["license"],
     description=package.get("description", package_name),
-    long_description=f"See <{url}>",
+    long_description=f"See <{repo_url}>",
     long_description_content_type='text/markdown',
     install_requires=[],
 )
