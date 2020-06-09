1ca0e4f618a153b99d452742820e2c0d9bd96641
noisycomputation <noisycomputation@gmail.com>
2020-06-09 01:22:04 -0600

    Make setup.py consistent with siblings

--- dash-renderer/setup.py
+++ dash-renderer/setup.py
@@ -1,17 +1,22 @@
-import json
 from setuptools import setup
+import io
+import json
 
-with open("package.json") as fp:
-    package = json.load(fp)
+with open("package.json") as f:
+    package = json.load(f)
+
+package_name = str(package["name"].replace(" ", "_").replace("-", "_"))
 
 setup(
-    name="dash_renderer-noisycomputation",
+    name=package_name,
     version=package["version"],
-    author="Chris Parmer",
-    author_email="chris@plotly.com",
-    packages=["dash_renderer"],
+    author=package["author"],
+    packages=[package_name.replace("_noisycomputation", "")],
+    url='https://github.com/noisycomputation/dash-renderer',
     include_package_data=True,
-    license="MIT",
-    description="Front-end component renderer for Dash",
+    license=package["license"],
+    description=package.get("description", package_name),
+    long_description=io.open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     install_requires=[],
 )
